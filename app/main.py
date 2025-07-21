b9c149c6ec0112656c865bffe71ba506bcf6a6f6
Aldo <aldo_roman13@hotmail.com>
2025-07-20 19:35:03 -0600

    Move table creation to startup event

--- app/main.py
+++ app/main.py
@@ -3,8 +3,17 @@ from app.api.routes import auth
 from app.db.models import Base
 from app.db.session import engine
 
-Base.metadata.create_all(bind=engine)
+app = FastAPI(
+    title="FASTAPI AUTH SERVICE",
+    version="1.0.0",
+    docs_url="/docs",
+    redoc_url="/redoc"
+)
 
-app = FastAPI()
+# Crear tablas al iniciar la app
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
 
+# Rutas de autenticación
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
