e906c9139890c06f0cfff972f4353149cb435f84
Aldo <aldo_roman13@hotmail.com>
2025-07-20 19:35:02 -0600

    Update User model to use hashed_password field

--- app/db/models.py
+++ app/db/models.py
@@ -8,6 +8,6 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
-    password = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)  
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     status = Column(Boolean, default=True)
